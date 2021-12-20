38f97c6fce654d7311e6c3e23aafacb1d787ee1a
Ashokkumar Khape <akhape@localhost.localdomain>
2021-12-20 13:15:21 +0530

    Added quartus executable path

--- quartus_DE10Nano.sh
+++ quartus_DE10Nano.sh
@@ -4,7 +4,7 @@ TOP_LEVEL_FILE=filtref.bdf
 FAMILY="Cyclone V"
 PART=EP3C10F256C8
 PACKING_OPTION=minimize_area
-quartus_map Hello --source=Hello.bdf --family="Cyclone V"
-quartus_fit Hello --part=5CSEBA6U23I7 --pack_register=minimize_area
-quartus_asm Hello
-quartus_sta Hello
+$QUARTUS/quartus_map Hello --source=Hello.bdf --family="Cyclone V"
+$QUARTUS/quartus_fit Hello --part=5CSEBA6U23I7 --pack_register=minimize_area
+$QUARTUS/quartus_asm Hello
+$QUARTUS/quartus_sta Hello   
