eddf8aa3652853483385731c7e7d3e5b9269eb49
Ashokkumar Khape <akhape@localhost.localdomain>
2021-12-26 22:43:03 +0530

    Updated tcl script

--- Hello.tcl
+++ Hello.tcl
@@ -20,24 +20,43 @@
 # Load Quartus Prime Tcl Project package
 package require ::quartus::project
 
+set need_to_close_project 0
+set make_assignments 1
+
+# Check that the right project is open
+if {[is_project_open]} {
+	if {[string compare $quartus(project) "Hello"]} {
+		puts "Project Hello is not open"
+		set make_assignments 0
+	}
+} else {
+	# Only open if not already open
+	if {[project_exists Hello]} {
+		project_open -revision Hello Hello
+	} else {
+		project_new -revision Hello Hello
+	}
+	set need_to_close_project 1
+}
 
 # Make assignments
+if {$make_assignments} {
+	set_global_assignment -name FAMILY "Cyclone V"
+	set_global_assignment -name DEVICE 5CSEBA6U23I7
+	set_global_assignment -name ORIGINAL_QUARTUS_VERSION 20.1.1
+	set_global_assignment -name VERILOG_FILE Hello.v
+	set_location_assignment PIN_W15 -to io_led
+	set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to io_led
+	set_location_assignment PIN_V11 -to clock
+	set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to clock
+	set_location_assignment PIN_AH17 -to reset
+	set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to reset
 
-set_global_assignment -name FAMILY "Cyclone V"
-set_global_assignment -name DEVICE 5CSEBA6U23I7
-set_global_assignment -name ORIGINAL_QUARTUS_VERSION 20.1.1
-set_global_assignment -name PROJECT_CREATION_TIME_DATE "12:05:33  DECEMBER 21, 2021"
-set_global_assignment -name LAST_QUARTUS_VERSION "20.1.1 Lite Edition"
-set_global_assignment -name VERILOG_FILE Hello.v
-set_location_assignment PIN_W15 -to io_led
-set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to io_led
-set_location_assignment PIN_V11 -to clock
-set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to clock
-set_location_assignment PIN_AH17 -to reset
-set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to reset
+	# Commit assignments
+	export_assignments
 
-# Commit assignments
-export_assignments
-
-
-
+	# Close project
+	if {$need_to_close_project} {
+		project_close
+	}
+}
